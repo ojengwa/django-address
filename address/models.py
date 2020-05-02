4fd80ea0729e9ad02a59dd602c3109e5f5ecc7aa
Bernard Ojengwa <ojengwa@users.noreply.github.com>
2020-05-02 11:47:48 +0100

    Update models.py

--- address/models.py
+++ address/models.py
@@ -4,7 +4,6 @@ import sys
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.fields.related import ForeignObject
-from django.utils.encoding import python_2_unicode_compatible
 
 try:
     from django.db.models.fields.related_descriptors import ForwardManyToOneDescriptor
@@ -189,7 +188,6 @@ class Country(models.Model):
 ##
 
 
-@python_2_unicode_compatible
 class State(models.Model):
     name = models.CharField(max_length=165, blank=True)
     code = models.CharField(max_length=3, blank=True)
@@ -215,7 +213,6 @@ class State(models.Model):
 ##
 
 
-@python_2_unicode_compatible
 class Locality(models.Model):
     name = models.CharField(max_length=165, blank=True)
     postal_code = models.CharField(max_length=10, blank=True)
@@ -245,7 +242,6 @@ class Locality(models.Model):
 ##
 
 
-@python_2_unicode_compatible
 class Address(models.Model):
     street_number = models.CharField(max_length=20, blank=True)
     route = models.CharField(max_length=100, blank=True)
